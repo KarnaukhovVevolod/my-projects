ec2aedc6235e271a9daf0014e4702c4929e900bb
vsevolod_restaurant <karnaukhov333@gmail.com>
2021-02-17 10:08:59 +0300

    add layout restaurant

--- module/Restaurant/config/module.config.php
+++ module/Restaurant/config/module.config.php
@@ -31,6 +31,10 @@ return[
         ],
     ],
     'view_manager' => [
+        'template_map' =>[
+            'layout/layout'     => __DIR__ . '/../view/layout/layout.phtml',
+            
+        ],
         'template_path_stack' => [
             __DIR__.'/../view',
         ],
